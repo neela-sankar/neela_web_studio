04ced94dd1ca012ed0e410d03af942da061c72e2
neela-sankar <neela.ecest@gmail.com>
2024-03-12 16:21:30 +0530

    file name changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemesProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const outfit = Outfit({ subsets: ["latin"] });
 
